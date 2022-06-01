99acf226988cbadb6115a1f5b42533351eea21df
Deepak Kumar <106160111+Deepak20131313@users.noreply.github.com>
2022-06-01 14:01:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
-    
-    $("h1").css("color", "green");
-});
+    $("#main").click(function(){
+      $("#sub_div").toggle();
+      
+      });
+   
+   });
+   
+   jQuery(function(){
+            jQuery('#showall').click(function(){
+                  jQuery('.targetDiv').show();
+           });
+           jQuery('.showSingle').click(function(){
+                 jQuery('.targetDiv').hide();
+                 jQuery('#div'+$(this).attr('target')).show();
+                  
+           });
+   });
+   
+   $(".buttons a").click(function() {
+   $(this).addClass('active').siblings().removeClass('active');
+   });
