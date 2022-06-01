ce109ec769e62721ac9c21d29623eaf182ebcd9c
Deepak Kumar <106160111+Deepak20131313@users.noreply.github.com>
2022-06-01 16:06:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ $(document).ready(function(){
    $(".buttons a").click(function() {
    $(this).addClass('active').siblings().removeClass('active');
    });
+
+
+   $(document).ready(function(){
+    $(".btn_hamr").click(function(){
+      $(".menu_mobile").toggle();
+    });
+   });
+   function myFunction(x) {
+       x.classList.toggle("change");
+     }
+ 
